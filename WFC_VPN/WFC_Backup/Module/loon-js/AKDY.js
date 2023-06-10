675507757c2c9b40b0bb83b10ed0425082a6123a
zhou yu <33508419+HMAU-AC@users.noreply.github.com>
2023-06-10 10:05:21 +0800

    uu

--- WFC_VPN/WFC_Backup/Module/loon-js/AKDY.js
+++ WFC_VPN/WFC_Backup/Module/loon-js/AKDY.js
@@ -1,5 +1,5 @@
 let regex_head='<head>';
-let Style='<head><style>.stui-pannel_hd,.stui-pannel-box.clearfix,.stui-pannel.stui-pannel-bg.hidden-sm.hidden-xs.clearfix,.stui-foot.clearfix,img#tj,{display: none!important;clip-path: polygon(0px 0px,0px 0px,0px 0px,0px 0px)!important;}</style>';
+let Style='<head><style>.stui-pannel_hd,.stui-pannel-box.clearfix,.stui-pannel.stui-pannel-bg.hidden-sm.hidden-xs.clearfix,.stui-foot.clearfix,img#tj{display: none!important;clip-path: polygon(0px 0px,0px 0px,0px 0px,0px 0px)!important;}</style>';
 
 let body=$response.body.replace(regex_head,Style);$done({body});
 
