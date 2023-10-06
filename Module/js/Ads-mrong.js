4bd4c6c8f66c9d264b63bf75cfe4fa7f4530a94f
zhou yu <33508419+HMAU-AC@users.noreply.github.com>
2023-10-07 03:23:32 +0800

    del

--- Module/js/Ads-mrong.js
+++
@@ -1,8 +0,0 @@
-let regex_head='<head>';
-let style=`<head><style>
-.share-post, iframe {
-display: none !important;
-}
-</style></head>`
-let body=$response.body.replace(regex_head,style);
-$done({body});
