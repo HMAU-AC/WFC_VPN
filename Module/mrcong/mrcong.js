694126449fec3ec1707e65c401ced4c1c0c6446a
zhou yu <33508419+HMAU-AC@users.noreply.github.com>
2023-10-09 10:35:43 +0800

    update

--- Module/mrcong/mrcong.js
+++ Module/mrcong/mrcong.js
@@ -1,4 +1,4 @@
-function {
+function myFunction(){
     // 使用document.getElementsByTagName方法获取到所有的iframe标签，并将其存储在一个NodeList中
 const iframes = document.getElementsByTagName('iframe');
 // 将NodeList转换为数组，并遍历删除每个iframe标签
