4474d3641a3c75fc477ce8e1c588719107fbe15f
HMAU-AC <1505130014@qq.com>
2023-10-09 09:50:12 +0800

    add mrcong.css/.js

---
+++ Module/mrcong/mrcong.js
@@ -0,0 +1,6 @@
+// 使用document.getElementsByTagName方法获取到所有的iframe标签，并将其存储在一个NodeList中
+const iframes = document.getElementsByTagName('iframe');
+// 将NodeList转换为数组，并遍历删除每个iframe标签
+Array.from(iframes).forEach((iframe) => {
+  iframe.remove();
+});
