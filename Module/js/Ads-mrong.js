f945b73225dac496dbfa657c4fef49a061e3cc5a
zhou yu <33508419+HMAU-AC@users.noreply.github.com>
2023-10-17 09:12:50 +0800

    ghgg

--- Module/js/Ads-mrong.js
+++ Module/js/Ads-mrong.js
@@ -31,30 +31,35 @@ div#sub-frame-error {display: none !important;}
 div#text-html-widget-6 {display: none !important;}
 </style>\
 <script id="remove" type='text/javascript'>
-{
   // 使用document.getElementsByTagName方法获取到所有的iframe标签，并将其存储在一个NodeList中
   const iframes = document.getElementsByTagName('iframe');
   // 将NodeList转换为数组，并遍历删除每个iframe标签
   Array.from(iframes).forEach((iframe) => {
     iframe.remove();
-  });
-    };
-{
+  };
+    
+</script>\    
+<script id="remove" type='text/javascript'>    
+    
   const disqus_count = document.getElementById('disqus_count-js-extra');
   disqus_count.remove();
-    };
-{
+</script>\    
+<script id="remove" type='text/javascript'>
+
   const tie = document.getElementById('tie-scripts-js-extra');
   tie.remove();
-    };
-{
+</script>\    
+<script id="remove" type='text/javascript'>
+
   const jetpack = document.getElementById('jetpack-stats-js-after');
   jetpack.remove();
-    };
-{
+</script>\    
+<script id="remove" type='text/javascript'>
+
   const jetpacks = document.getElementById('jetpack-stats-js');
   jetpacks.remove();
-    };
+</script>\    
+<script id="remove" type='text/javascript'>
 /* 
     // 删除所有没有id的 scripts
 {
@@ -67,7 +72,8 @@ div#text-html-widget-6 {display: none !important;}
 */ 
 
 // 删除所有a标签中的about:blank
-{
+</script>\    
+<script id="remove" type='text/javascript'>
   // 获取所有标签
 const tabs = document.querySelectorAll('a');
 
@@ -78,11 +84,37 @@ tabs.forEach(tab => {
   // 删除标签
     tab.remove();
   }
-});
+}
+</script>\    
+
+<script id="remove" type='text/javascript'>
+// 删除所有span标签中的about:blank    
+
+// 获取所有的<span>标签
+const spans = document.querySelectorAll('span');
+
+  // 遍历每个<span>标签
+spans.forEach(span => {
+  // 删除<span>标签
+  span.remove();
+}
+
+    </script>\    
+<script id="remove" type='text/javascript'>
+      const ad = document.getElementById('__stay_inject_selecte_ad_tag_js_');
+      ad.remove();
     };
     
+</script>\
+
+<script id="removevd" type='text/javascript'>
+const advd = document.getElementById('__stay_inject_parse_video_js');
+      advd.remove();
+</script>\
+<script id="remove" type='text/javascript'>
+    
 // 删除所有img标签中的about:blank
-{
+
 // 获取所有img标签
 const images = document.querySelectorAll('img');
 
@@ -93,30 +125,8 @@ images.forEach(image => {
   // 删除img标签
     image.remove();
   }
-});
-    };
-// 删除所有span标签中的about:blank    
-{
-// 获取所有的<span>标签
-const spans = document.querySelectorAll('span');
-
-  // 遍历每个<span>标签
-spans.forEach(span => {
-  // 删除<span>标签
-  span.remove();
-});
-
-    };
-{
-      const ad = document.getElementById('__stay_inject_selecte_ad_tag_js_');
-      ad.remove();
-    };
-    
-</script>\
-<script id="removevd" type='text/javascript'>
-const advd = document.getElementById('__stay_inject_parse_video_js');
-      advd.remove();
-</script>\
+};
+</script>\    
 
 `
 
