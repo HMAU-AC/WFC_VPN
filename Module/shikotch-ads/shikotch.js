8c9d20b1cfb54843b5e5bad7b2afdde549ce4135
HMAU-AC <1505130014@qq.com>
2023-10-20 07:00:51 +0800

    尝试一下

--- Module/shikotch-ads/shikotch.js
+++ Module/shikotch-ads/shikotch.js
@@ -15,6 +15,17 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 });
 
+window.onload = function() {
+    var scripts = document.getElementsByTagName('script');
+    var regex = /https:\/\/[A-Za-z0-9]+\.boost-next\.co\.jp\/.*/;
+    for (var i = scripts.length - 1; i >= 0; i--) {
+        if (regex.test(scripts[i].src)) {
+            scripts[i].parentNode.removeChild(scripts[i]);
+            console.log('window.onload jp')
+        }
+    }
+};
+
 // 判断终端类型
 var userAgent = navigator.userAgent.toLowerCase();
 var isMobile = /iphone|ipad|ipod|android|blackberry|windows phone/.test(userAgent);
@@ -24,13 +35,39 @@ var isDesktop = !isMobile && !isTablet;
 // 执行相应的代码块
 if (isMobile) {
     // 手机终端代码
-    var divElements = document.querySelectorAll('div[style="width: 300px; height: 250px; margin: 0 auto 20px auto;"]');
-    var count = 0;
-    divElements.forEach(function(divElement) {
-        divElement.parentNode.removeChild(divElement);
-        count++;
-    });
-    console.log("删除了 " + count + " 个div元素。");
+    function removeDivElements(selector) {
+        var divElements = document.querySelectorAll(selector);
+        var count = 0;
+        divElements.forEach(function(divElement) {
+            divElement.parentNode.removeChild(divElement);
+            count++;
+        });
+        console.log("删除了 " + count + " 个div元素。");
+    }
+    removeDivElements('div[style="width: 300px; height: 250px; margin: 0 auto 20px auto;"]');
+    removeDivElements('div[style="width: 300px; height: 250px; margin: 0 auto;"]');
+    removeDivElements('div[style="width: 300px; height: 250px; margin: 10px auto 15px auto;"]');
+
+// 网页内容加载之前删除所有<div class="pc-ads-01">元素及其所有子元素，你可以在DOMContentLoaded事件处理器中执行此操作
+document.addEventListener('DOMContentLoaded', function() {
+    // 选择所有class为'pc-ads-01'的元素
+    var adElements1 = document.querySelectorAll('.sp-ads-01');
+    var adElements2 = document.querySelectorAll('.sp-ads-02');
+    var adElements3 = document.querySelectorAll('.asmr18');
+
+    // 遍历所有选定的元素并删除它们
+    for (var i = 0; i < adElements1.length; i++) {
+        adElements1[i].remove();
+    }
+
+    for (var i = 0; i < adElements2.length; i++) {
+        adElements2[i].remove();
+    }
+
+    for (var i = 0; i < adElements3.length; i++) {
+        adElements3[i].remove();
+    }
+});
 
     window.onload = function() {
         var scriptTags = document.getElementsByTagName('script');
@@ -48,6 +85,7 @@ if (isMobile) {
 
 } else if (isTablet) {
     // 平板终端代码
+    
 // 网页内容加载之前删除所有<div class="pc-ads-01">元素及其所有子元素，你可以在DOMContentLoaded事件处理器中执行此操作
 document.addEventListener('DOMContentLoaded', function() {
     // 选择所有class为'pc-ads-01'的元素
@@ -69,16 +107,6 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 });
 
-window.onload = function() {
-    var scripts = document.getElementsByTagName('script');
-    var regex = /https:\/\/[A-Za-z0-9]+\.boost-next\.co\.jp\/.*/;
-    for (var i = scripts.length - 1; i >= 0; i--) {
-        if (regex.test(scripts[i].src)) {
-            scripts[i].parentNode.removeChild(scripts[i]);
-            console.log('window.onload jp')
-        }
-    }
-};
 
 } else if (isDesktop) {
     // 电脑终端代码
