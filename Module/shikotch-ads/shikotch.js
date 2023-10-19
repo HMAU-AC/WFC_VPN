f224acce4a633502d4c831d94930f361adcf6e6a
HMAU-AC <1505130014@qq.com>
2023-10-20 00:47:47 +0800

    fix bug

--- Module/shikotch-ads/shikotch.js
+++ Module/shikotch-ads/shikotch.js
@@ -93,10 +93,22 @@ observer.observe(document.body, config);
 
 // 根据scripts中的scr“”中的链接来循环定位删除scripts标签
 // 由于在循环过程中删除元素可能会导致索引问题，你可能需要从后向前遍历<script>标签
-var scripts = document.getElementsByTagName('script');
-var regex = /https:\/\/js\.boost-next\.co\.jp\/.*/;
-for (var i = scripts.length - 1; i >= 0; i--) {
-    if (regex.test(scripts[i].src)) {
-        scripts[i].parentNode.removeChild(scripts[i]);
+document.addEventListener('DOMContentLoaded', function() {
+    var scripts = document.getElementsByTagName('script');
+    var regex = /https:\/\/js\.boost-next\.co\.jp\/.*/;
+    for (var i = scripts.length - 1; i >= 0; i--) {
+        if (regex.test(scripts[i].src)) {
+            scripts[i].parentNode.removeChild(scripts[i]);
+        }
     }
-}
+});
+
+window.onload = function() {
+    var scripts = document.getElementsByTagName('script');
+    var regex = /https:\/\/js\.boost-next\.co\.jp\/.*/;
+    for (var i = scripts.length - 1; i >= 0; i--) {
+        if (regex.test(scripts[i].src)) {
+            scripts[i].parentNode.removeChild(scripts[i]);
+        }
+    }
+};
