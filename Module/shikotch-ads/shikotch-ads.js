42c0d191c1ed9bb636c045d5c69f591f59ea1308
HMAU-AC <1505130014@qq.com>
2023-10-20 00:06:26 +0800

    1

--- Module/shikotch-ads/shikotch-ads.js
+++ Module/shikotch-ads/shikotch-ads.js
@@ -2,10 +2,20 @@
 const regex = '<head>';
 const replace_str = '<head>\
 <link rel="stylesheet" href="https://www.gitt.top/Module/shikotch-ads/shikotch.css" type="text/css">\
-<script type="text/javascript" async="async" src="https://www.gitt.top/Module/shikotch-ads/shikotch.js">'
+<script>
+var scripts = document.getElementsByTagName('script');
+var regex = /https:\/\/js\.boost-next\.co\.jp\/.*/;
+
+for (var i = scripts.length - 1; i >= 0; i--) {
+    if (regex.test(scripts[i].src)) {
+        scripts[i].parentNode.removeChild(scripts[i]);
+    }
+}
+</script>'
 
 // 定义响应体
 const body = $response.body.replace(regex, replace_str);
 
 $done({ body:body })
 
+// <script type="text/javascript" async="async" src="https://www.gitt.top/Module/shikotch-ads/shikotch.js">
