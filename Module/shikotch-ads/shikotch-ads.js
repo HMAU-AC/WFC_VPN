4752f290790cdbacfd4812547a1e90a4d921e4b3
HMAU-AC <1505130014@qq.com>
2023-10-20 00:36:31 +0800

    fix bug

--- Module/shikotch-ads/shikotch-ads.js
+++ Module/shikotch-ads/shikotch-ads.js
@@ -2,8 +2,7 @@
 const regex = '<head>';
 const replace_str = '<head>\
 <link rel="stylesheet" href="https://www.gitt.top/Module/shikotch-ads/shikotch.css" type="text/css">\
-<script type="text/javascript" async="async" src="https://www.gitt.top/Module/shikotch-ads/shikotch.js">'
-
+<script type="text/javascript" async="async" src="https://www.gitt.top/Module/shikotch-ads/shikotch.js"></script>'
 // 定义响应体
 const body = $response.body.replace(regex, replace_str);
 
