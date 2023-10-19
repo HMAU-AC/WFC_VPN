28d38332b3ff6d8a6df05b2a757adaa72a211890
HMAU-AC <1505130014@qq.com>
2023-10-19 11:02:34 +0800

    fix update crong.js

--- Module/js/Ads-mrong.js
+++ Module/js/Ads-mrong.js
@@ -90,7 +90,14 @@ for (var i = 0; i < scriptstoads.length; i++) {
         scriptstoads[i].parentNode.removeChild(scriptstoads[i]);
     }
 }
-
+// 这段代码会遍历页面上的所有元素，为每个元素创建一个克隆，然后用克隆替换原始元素。由于克隆不会复制事件监听器，所以这将删除所有的事件监听器
+// 实际的广告可能使用更复杂的方法来触发，例如使用setTimeout或setInterval，或者动态添加事件监听器。要处理这些情况，你可能需要更复杂的解决方案，例如使用浏览器扩展或者网络代理来拦截和修改JavaScript代码
+// 下面的并不一定管用
+var allElements = document.getElementsByTagName('*');
+for (var i = 0; i < allElements.length; i++) {
+    var elementClone = allElements[i].cloneNode(true);
+    allElements[i].parentNode.replaceChild(elementClone, allElements[i]);
+}
 </script>`
 
 let body = $response.body.replace(regex_head,style);
