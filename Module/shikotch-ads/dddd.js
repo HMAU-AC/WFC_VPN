b244b8fa77683d27300fcd2ca8c9e25558eebf50
HMAU-AC <1505130014@qq.com>
2023-10-20 00:26:45 +0800

    1

--- Module/shikotch-ads/dddd.js
+++ Module/shikotch-ads/dddd.js
@@ -1,5 +1,14 @@
 
-/*
+// 根据scripts中的scr“”中的链接来循环定位删除scripts标签
+// 由于在循环过程中删除元素可能会导致索引问题，你可能需要从后向前遍历<script>标签
+var scripts = document.getElementsByTagName('script');
+var regex = /https:\/\/js\.boost-next\.co\.jp\/.*/;
+for (var i = scripts.length - 1; i >= 0; i--) {
+    if (regex.test(scripts[i].src)) {
+        scripts[i].parentNode.removeChild(scripts[i]);
+    }
+}
+
 // 替换iframes scr""中的链接为about:blank
 var iframes = document.getElementsByTagName('iframe');
 for (var i = 0; i < iframes.length; i++) {
@@ -93,4 +102,3 @@ var config = { childList: true, subtree: true };
 // 传入目标节点和观察选项
 observer.observe(document.body, config);
 
-*/
--- Module/shikotch-ads/shikotch.js
+++ Module/shikotch-ads/shikotch.js
@@ -1,10 +1,4 @@
-// 根据scripts中的scr“”中的链接来循环定位删除scripts标签
-// 由于在循环过程中删除元素可能会导致索引问题，你可能需要从后向前遍历<script>标签
-var scripts = document.getElementsByTagName('script');
-var regex = /https:\/\/js\.boost-next\.co\.jp\/.*/;
-
-for (var i = scripts.length - 1; i >= 0; i--) {
-    if (regex.test(scripts[i].src)) {
-        scripts[i].parentNode.removeChild(scripts[i]);
-    }
-}
+const iframesToRemove = document.querySelectorAll('iframe');
+iframesToRemove.forEach(iframe => {
+    iframe.remove();
+});
