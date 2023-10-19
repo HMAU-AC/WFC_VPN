4fc2f4935cb94f87c0d3c16e6d2ed8081d1069e9
HMAU-AC <1505130014@qq.com>
2023-10-20 00:27:47 +0800

    1

--- Module/shikotch-ads/shikotch-ads.js
+++ Module/shikotch-ads/shikotch-ads.js
@@ -1,10 +1,10 @@
 // 定义 CSS/JS
 const regex = '<head>';
 const replace_str = '<head>\
-<link rel="stylesheet" href="https://www.gitt.top/Module/shikotch-ads/shikotch.css" type="text/css">\
 <script type="text/javascript" async="async" src="https://www.gitt.top/Module/shikotch-ads/shikotch.js">'
 
 // 定义响应体
 const body = $response.body.replace(regex, replace_str);
 
 $done({ body:body })
+// <link rel="stylesheet" href="https://www.gitt.top/Module/shikotch-ads/shikotch.css" type="text/css">\
