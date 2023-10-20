3df251df8bc0db1b76b11bce6c453a51cbe54c85
HMAU-AC <1505130014@qq.com>
2023-10-20 12:35:42 +0800

    更新

--- generate_html.py
+++ generate_html.py
@@ -1,16 +1,22 @@
 import os
 
-def generate_html(root_dir):
-    html = ["<ul>"]
+def generate_links(root_dir):
+    links = []
     for foldername, subfolders, filenames in os.walk(root_dir):
         for filename in filenames:
-            if filename != 'README.md':
-                path = os.path.join(foldername, filename)
-                html.append(f'<li><a href="{path}">{filename}</a></li>')
-    html.append("</ul>")
-    return '\n'.join(html)
+            path = os.path.join(foldername, filename)
+            links.append(f'<a href="{path}">{filename}</a>')
+    return '\n'.join(links)
 
 if __name__ == "__main__":
-    html = generate_html('.')
+    links = generate_links('.')
+    html_content = f"""
+    <!DOCTYPE html>
+    <html>
+    <body>
+    {links}
+    </body>
+    </html>
+    """
     with open('index.html', 'w') as file:
-        file.write(html)
+        file.write(html_content)
