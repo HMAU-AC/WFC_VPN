c49627422abe3d6990ef2db9b1b69c58a3daf006
HMAU-AC <1505130014@qq.com>
2023-10-20 14:55:57 +0800

    update yml

--- generate_html.py
+++ generate_html.py
@@ -9,14 +9,14 @@ def generate_links(root_dir, repo_url, branch_name, ignore_files=None):
             if any(filename.endswith(ext) for ext in ignore_files):
                 continue
             path = os.path.join(foldername, filename).replace("\\", "/")
-            url = f"{repo_url}/blob/{branch_name}/{path}"
+            url = f"{repo_url}/WFC_VPN/{branch_name}/{path}"
             links.append(f'<a href="{url}">{filename}</a>')
     return '\n'.join(links)
 
 if __name__ == "__main__":
-    repo_url = "https://raw.githubusercontent.com/HMAU-AC/WFC_VPN/main"  # 替换为你的仓库 URL
+    repo_url = "https://raw.githubusercontent.com/HMAU-AC"  # 替换为你的仓库 URL
     branch_name = "main"  # 替换为你的分支名
-    links = generate_links('.', repo_url, branch_name, ignore_files=['.pyc', '.git', '.gitignore', '.github'])
+    links = generate_links('.', repo_url, branch_name, ignore_files=['.pyc', '.git', '.gitignore', '.github', 'html.css', 'index.html', 'generate_html.py'])
     html_content = f"""
 <!DOCTYPE html>
 <html>
