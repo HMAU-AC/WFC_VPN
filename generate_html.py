261abffe1ee4c75c0b2e6b3146a1c2ecd570f300
HMAU-AC <1505130014@qq.com>
2023-10-20 14:33:47 +0800

    update yml

--- generate_html.py
+++ generate_html.py
@@ -14,12 +14,39 @@ if __name__ == "__main__":
     branch_name = "main"  # 替换为你的分支名
     links = generate_links('.', repo_url, branch_name)
     html_content = f"""
-    <!DOCTYPE html>
-    <html>
-    <body>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>My Page</title>
+    <style>
+        body {{
+            font-family: Arial, sans-serif;
+            margin: 0;
+            padding: 0;
+            background-color: #f0f0f0;
+        }}
+        .container {{
+            width: 80%;
+            margin: auto;
+            background-color: #fff;
+            padding: 20px;
+            box-shadow: 0px 0px 10px 0px rgba(0,0,0,0.1);
+        }}
+        a {{
+            color: #333;
+            text-decoration: none;
+        }}
+        a:hover {{
+            color: #0056b3;
+        }}
+    </style>
+</head>
+<body>
+    <div class="container">
     {links}
-    </body>
-    </html>
-    """
+    </div>
+</body>
+</html>
+"""
     with open('index.html', 'w') as file:
         file.write(html_content)
