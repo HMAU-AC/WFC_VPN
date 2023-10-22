4c185bddc0028c60b32ed7222c30aa1cae81ca1c
HMAU-AC <1505130014@qq.com>
2023-10-22 09:36:55 +0800

    333

--- generate_html.py
+++ generate_html.py
@@ -80,10 +80,9 @@ if __name__ == "__main__":
             </div>
         </div>
     </div>
-    <!-- 引入jQuery -->
-    <script rel="preconnect" src="https://cdn.bootcdn.net/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
     <!-- 引入Bootstrap JS -->
-    <script rel="preconnect" src="https://cdn.bootcdn.net/ajax/libs/twitter-bootstrap/4.3.1/js/bootstrap.min.js"></script>
+    <script src="https://cdn.bootcdn.net/ajax/libs/twitter-bootstrap/4.3.1/js/bootstrap.min.js"></script>
 </body>
 </html>
 """
