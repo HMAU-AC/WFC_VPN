dd164e1ac57cf498fde517c3b76f006f5653f09c
HMAU-AC <1505130014@qq.com>
2023-10-22 11:36:06 +0800

    快了还有40个

--- generate_html.py
+++ generate_html.py
@@ -17,7 +17,7 @@ def generate_links(root_dir, repo_url, ignore_files=None):
                 continue
             if filename:  # 检查filename是否为空
                 file_url = f"{repo_url}/{file_path}"  # 修改了这里
-                links.append(f'<a class="list-group-item d-inline-block" href="{file_url}" >{filename}</a><button class="btn btn-secondary btn-copy float-right" data-clipboard-text="{file_url}"><i class="fas fa-copy"></i></button>')  # 修改了这里
+                links.append(f'<div class="d-flex justify-content-between align-items-center"><a class="list-group-item flex-grow-1" href="{file_url}" >{filename}</a><button class="btn btn-secondary btn-copy" data-clipboard-text="{file_url}"><i class="fas fa-copy"></i></button></div>')  # 修改了这里
     return '\n'.join(links)
 
 
