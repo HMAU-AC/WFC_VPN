a0fc6936d5a9db5b0261b3e12bfb516551da7868
HMAU-AC <1505130014@qq.com>
2023-10-22 09:28:20 +0800

    333

--- generate_html.py
+++ generate_html.py
@@ -32,8 +32,10 @@ if __name__ == "__main__":
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>文件目录</title>
     <!-- 引入Bootstrap CSS -->
+    <link rel="preconnect" href="https://cdn.bootcdn.net/ajax/libs/twitter-bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet">
     <link rel="dns-prefetch" href="https://cdn.bootcdn.net/ajax/libs/twitter-bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet">
     <!-- 引入Font Awesome -->
+    <link rel="preconnect" href="https://cdn.bootcdn.net/ajax/libs/font-awesome/5.15.3/css/all.min.css" rel="stylesheet">
     <link rel="dns-prefetch" href="https://cdn.bootcdn.net/ajax/libs/font-awesome/5.15.3/css/all.min.css" rel="stylesheet">
     <style>
         .folder-label {{
@@ -80,6 +82,7 @@ if __name__ == "__main__":
         </div>
     </div>
     <!-- 引入Bootstrap JS -->
+    <script rel="preconnect" src="https://cdn.bootcdn.net/ajax/libs/twitter-bootstrap/4.3.1/js/bootstrap.min.js"></script>
     <script rel="dns-prefetch" src="https://cdn.bootcdn.net/ajax/libs/twitter-bootstrap/4.3.1/js/bootstrap.min.js"></script>
 </body>
 </html>
