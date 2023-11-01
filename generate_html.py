f4261bf16ae8b69289215f1ab4cfb3a24a40af9a
HMAU-AC <1505130014@qq.com>
2023-11-01 08:04:01 +0800

    修改py

--- generate_html.py
+++ generate_html.py
@@ -63,6 +63,8 @@ if __name__ == "__main__":
         position: fixed;
         top: 0;
         left: 0;
+        right: 0;
+        bottom: 0;
         width: 100%;
         height: 100%;
         background-size: cover;
