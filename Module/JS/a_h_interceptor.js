225a85d23e8bdf9effa37c04a333bdde639fd8a4
zhou yu <33508419+HMAU-AC@users.noreply.github.com>
2024-05-19 22:18:18 +0800

    Create a_h_interceptor.js

---
+++ Module/JS/a_h_interceptor.js
@@ -0,0 +1,10 @@
+// 重定向到 GitHub 上的 HTML 文件
+$done({
+    response: {
+        status: 302,
+        headers: { 
+            'Location': 'https://raw.githubusercontent.com/HMAU-AC/WFC_VPN/main/Module/JS/index.html',
+            'Content-Type': 'text/html'
+        }
+    }
+});
