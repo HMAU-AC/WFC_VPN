caab8d5c84a27f18cb8841638eee93f4d4e57385
zhou yu <33508419+HMAU-AC@users.noreply.github.com>
2024-08-08 07:39:36 +0800

    了

--- Module/JS/google-User-Agent.js
+++
@@ -1,5 +0,0 @@
-// your-script.js
-let modifiedHeaders = $request.headers;
-modifiedHeaders['User-Agent'] = 'Mozilla/5.0 (Linux; Android 10; Mobile; rv:89.0) Gecko/89.0 Firefox/89.0';
-
-$done({headers: modifiedHeaders});
